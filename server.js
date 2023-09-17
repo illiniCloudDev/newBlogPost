648f28d79246abcb7067e86295e2a3e9faf7dd53
illiniCloudDev <107890959+illiniCloudDev@users.noreply.github.com>
2023-09-17 18:03:55 -0500

    finished backend server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('Connected you are my boy'))
+    .then(() => console.log('Connected to daddy MondgoDB!!'))
     .catch(error => console.log(error.message))
 
 const postSchema = new mongoose.Schema({
@@ -27,13 +27,29 @@ const Post = mongoose.model('Post', postSchema)
 //create new post
 //delete post
 
+//get all posts
 app.get('/posts', async (req, res) =>{
-    const posts= await Post.find()
+    const posts = await Post.find()
     res.send(posts)
 })
 
-app.get('/posts/:id', async (req, res) =>{
-    const posts= await Post.findById()
-    res.send(posts)
+//get one post
+app.get('/posts/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    res.send(post)
 })
 
+//create new post
+app.post('/posts', async (req, res) => {
+    const newPost = new Post(req.body)
+    const savedPost = await newPost.save()
+    res.send(savedPost)
+})
+
+//delete post
+app.delete('/posts/:id', async (req, res) =>{
+    await Post.findByIdAndDelete(req.params.id)
+    res.status(200).send('Post exterminated')
+})
+
+app.listen(5500, () => console.log('Server is looking good on port 5500'))
